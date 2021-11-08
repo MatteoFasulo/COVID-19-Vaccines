29c04a7f31ec34112685cd8a095cd03ee43a5ac5
Fasada <38442915+Fasada@users.noreply.github.com>
2021-11-08 22:46:52 +0100

    source utils

--- code.R
+++ code.R
@@ -1,1 +1,8 @@
-#
+source("https://raw.githubusercontent.com/MatteoFasulo/Rare-Earth/main/util/coreFunctions.R")
+loadPackages(libraries=c("stargazer","plm"))
+################################################################################
+
+
+descrStats <- stargazer(type="text", summary = T, title = "Descriptive statistics.", align = T, )
+
+CDTest <- pcdtest(x, test = c("cd","lm"))
