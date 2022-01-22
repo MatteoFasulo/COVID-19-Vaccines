90401f2bf921209d8b11727d6b7b6eaa427686eb
Matteo Fasulo <74818541+MatteoFasulo@users.noreply.github.com>
2022-01-22 16:52:55 +0100

    Add files via upload

--- Python/main.py
+++ Python/main.py
@@ -1,11 +1,62 @@
-import os
+import pandas as pd
+import seaborn as sns
+import statsmodels.formula.api as smf
+import matplotlib.pyplot as plt
+import constants as const
 
 import util
 
 def main():
-  print("All good, you can go")
+  """
+  """
+  # Setup Seaborn plot settings
+  sns.set(style="white")
+  sns.set(style="whitegrid", color_codes=True)
+  sns.set(rc={'figure.figsize':(11.7,13.27)})
+
+  # Get latest vaccine report from Protezione Civile
+  util.getLatestVaccineReport()
+
+  # Load the dataset using Pandas and plot first chart
+  fullDF = util.loadVaccines()
+  fullDF.groupby("fornitore")["prima_dose"].sum().plot.pie(autopct='%.2f%%',
+                                                         ylabel='',
+                                                         title='Percentage of first doses by manufacturer')
+  plt.savefig('1st_dose.png')
+
+  # Clean the dataset for all the following analysis
+  vaccinati = util.preProcessing(fullDF)
+  vaccinati.groupby("fornitore")["seconda_dose"].sum().plot.pie(autopct='%.2f',
+                                                                ylabel='',
+                                                                title='Percentage of second doses (vaccinated) by manufacturer')
+  plt.savefig('2st_dose.png')
+
+  sns.set(rc={'figure.figsize':(16,8)})
+  vaccinati.groupby(["nome_area","fornitore"])["seconda_dose"].sum().unstack().plot.bar(xlabel='Regione',
+                                                                                        title='Vaccine manufacturer by region')
+  plt.savefig('vaccine_x_manufacturer.png')
+
+  sns.set(rc={'figure.figsize':(16,8)})
+  vaccinati.groupby(["fascia_anagrafica","fornitore"])["seconda_dose"].sum().unstack().plot.bar(xlabel="Fascia di età",
+                                                                                                title='Vaccine manufacturer by age group')
+  plt.savefig('vaccine_x_age.png')
   
+  sns.set(rc={'figure.figsize':(20,50)})
+  vaccinati.groupby(["nome_area","fascia_anagrafica","fornitore"])["seconda_dose"].sum().unstack().plot(kind='barh', stacked=True,
+                                                                                                        ylabel="Regione, Fascia di età",
+                                                                                                        title='Vaccine manufacturer by region and age group')
+  plt.savefig('vaccine_x_region_and_age.png')
+
+  totOver12 = const.POP_ITA - const.POP_ITA_U12
+  vaccinati.groupby(['nome_area'])['seconda_dose'].apply(lambda x: (sum(x)/totOver12)*100)
+
+  dictFactorsDF = util.SVI()
+  finalDF = pd.merge(dictFactorsDF, vaccinati, left_index=True, right_on='codice_regione_ISTAT')
+  finalDF['coverage'] = finalDF.apply(lambda x: (x.tot_Dosi/x.residenti), axis=1)
+  finalDF = finalDF[['data_somministrazione', 'seconda_dose', 'tot_Dosi', 'residenti', 'coverage', 'fascia_anagrafica', 'fornitore', 'codice_regione_ISTAT','nome_area','PIL','mono_Family','six_more_Family','low_Educ','ass_Unease','house_Crow','u30_Unemployed','eco_Unease']]
+  finalDF = finalDF.reset_index(drop=True)
+  finalDF.describe()
+
 
 if __name__ == "__main__":
-  if util.getLatestVaccineReport() is True:
-    main()
+  main()
--- Python/util.py
+++ Python/util.py
@@ -1,8 +1,37 @@
+from genericpath import exists
 import os
 import urllib.request
 import pandas as pd
+import json
+import statsmodels.formula.api as smf
+import numpy as np
 
 import constants as const
+
+class NpEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        if isinstance(obj, np.floating):
+            return float(obj)
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return super(NpEncoder, self).default(obj)
+
+def loadVaccines(path=f'data{os.sep}latest.csv'):
+    fullDF = pd.read_csv(path)
+    
+    return fullDF
+
+def preProcessing(neededDF):
+    vaccinati = neededDF.drop(['area', 'sesso_maschile','sesso_femminile','prima_dose','pregressa_infezione','dose_addizionale_booster','codice_NUTS1','codice_NUTS2'], axis=1)
+    vaccinati = vaccinati.loc[(vaccinati["seconda_dose"] > 0)].reset_index(drop=True)
+    vaccinati['nome_area'].loc[(vaccinati["nome_area"] == 'Provincia Autonoma Trento')] = 'Provincia Autonoma Bolzano / Bozen'
+    vaccinati.loc[(vaccinati["nome_area"] == 'Provincia Autonoma Bolzano / Bozen')]
+    vaccinati['nome_area'].loc[(vaccinati["nome_area"] == 'Provincia Autonoma Bolzano / Bozen')] = 'Trentino Alto Adige'
+    vaccinati.loc[(vaccinati["nome_area"] == 'Trentino Alto Adige')]
+
+    return vaccinati
 
 def getLatestVaccineReport(url=const.LATEST_REPORT_URL, dir='data', filename='latest.csv'):
     if not os.path.isdir(dir):
@@ -20,73 +49,233 @@ def getLatestVaccineReport(url=const.LATEST_REPORT_URL, dir='data', filename='la
 
     return True
 
-"""def preProcessing(csvFile: str = 'data/latest.csv'):
-    assert(getLatestVaccineReport())
-    df = pd.read_csv(csvFile)
-    secondaDose = df.loc[(df["seconda_dose"] > 0)]
-    return secondaDose
+def regTotalVaccines(region):
+    fullDF = loadVaccines()
+    df = preProcessing(neededDF=fullDF)
+    if isinstance(region, int) is True:
+        val = df.loc[df['codice_regione_ISTAT'] == region]['seconda_dose'].sum()
+        reg = df.loc[df['codice_regione_ISTAT'] == region]['nome_area'].iloc[0]
+    else:
+        val = df.loc[df['nome_area'] == region]['seconda_dose'].sum()
+        reg = region
+    #print(f"La regione {reg} ha un totale di {val} vaccinati")
+    return val
 
-def regionVax(dataframe, result: bool = True):
-    totPopolazioneItaliana = const.POP_ITA
-    totUnder12 = const.POP_ITA_U12
-    totOver12 = totPopolazioneItaliana - totUnder12
-
-    if result is True:
-        percentages = dataframe.groupby(['nome_area'])['seconda_dose'].apply(lambda x: (sum(x)/totPopolazioneItaliana)*100)
-        return percentages
-
-def fetchIVSM(fileName: str = "data/2011.xlsx", header=None):
-    IVSM = pd.read_excel(fileName,header=header)
-    trentino = IVSM.loc[IVSM[5]=='Trentino-Alto Adige/Südtirol']
-    IVSM = pd.concat([IVSM,trentino],ignore_index=True)
-    bolzanoIDX = IVSM.loc[IVSM[5]=='Trentino-Alto Adige/Südtirol'].index.tolist()[0]
-    trentoIDX = IVSM.loc[IVSM[5]=='Trentino-Alto Adige/Südtirol'].index.tolist()[1]
-    IVSM[5][bolzanoIDX] = 'Provincia Autonoma Trento'
-    IVSM[5][trentoIDX] = 'Provincia Autonoma Bolzano'
-    ivsmList = IVSM.groupby([5])[96].sum().tolist()
-    return(ivsmList)
-
-def vaccineData(dataframe):
-    totPopolazioneItaliana = const.POP_ITA
-    regionList = dataframe['nome_area'].unique().tolist()
-    percentVaccinated = dataframe.groupby(['nome_area'])['seconda_dose'].apply(lambda x: (sum(x)/totPopolazioneItaliana)).tolist()
-    totMale = dataframe.groupby(['nome_area'])['sesso_maschile'].sum().tolist()
-    totFemale = dataframe.groupby(['nome_area'])['sesso_femminile'].sum().tolist()
-    totPregressi = dataframe.groupby(['nome_area'])['pregressa_infezione'].sum().tolist()
-    perEtaList = dataframe.groupby(['fascia_anagrafica','nome_area'])['seconda_dose'].sum().tolist()
-    ivsmList = fetchIVSM()
+def SVI():
+    """
+    Function to load the stored value for Social Vulnerable Index and PIL of region
+    """
+    if os.path.isfile(f'data{os.sep}regionSVI_PIL.json') is True:
+        exists = True
+    else:
+        exists = False
 
 
-    values = {
-    'region' : sorted(regionList), 
-    'percentVaccinated' : percentVaccinated,
-    'eta1': perEtaList[0:21],
-    'eta2': perEtaList[21:42],
-    'eta3': perEtaList[42:63],
-    'eta4': perEtaList[63:84],
-    'eta5': perEtaList[84:105],
-    'eta6': perEtaList[105:126],
-    'eta7': perEtaList[126:147],
-    'eta8': perEtaList[147:168],
-    'eta9': perEtaList[168:],
-    'male': totMale,
-    'female': totFemale,
-    'totPregressi':totPregressi,
-    'IVSM' : ivsmList
-    }
+    if exists is False:
+        dictFactors = {
+        1: {'mono_Family': 6.8, 
+                'six_more_Family': 0.8, 
+                'low_Educ': 1.3,
+                'ass_Unease': 3.2,
+                'house_Crow': 1.1,
+                'u30_Unemployed': 8.9,
+                'eco_Unease': 1.4,
+        'tot_Dosi': regTotalVaccines(1), 
+                'residenti': 4274945,
+        'PIL' : 126374.6},
+        2: {'mono_Family': 7.2, 
+                'six_more_Family': 0.7, 
+                'low_Educ': 1.0,
+                'ass_Unease': 2.8,
+                'house_Crow': 1.2,
+                'u30_Unemployed': 8.3,
+                'eco_Unease': 1.0,
+        'tot_Dosi': regTotalVaccines(2), 
+                'residenti': 124089,
+        'PIL' : 4522.4},
+        3: {'mono_Family': 6.3, 
+                'six_more_Family': 1.1, 
+                'low_Educ': 1.4,
+                'ass_Unease': 2.7,
+                'house_Crow': 1.2,
+                'u30_Unemployed': 8.1,
+                'eco_Unease': 1.2,
+        'tot_Dosi': regTotalVaccines(3), 
+                'residenti': 9981554,
+        'PIL' : 367167.2},
+        4: {'mono_Family': 7.1, 
+                'six_more_Family': 1.8, 
+                'low_Educ': 0.8,
+                'ass_Unease': 2.6,
+                'house_Crow': 1.1,
+                'u30_Unemployed': 6.7,
+                'eco_Unease': 0.9,
+        'tot_Dosi': regTotalVaccines(4), 
+                'residenti': 1077078,
+        'PIL' : 43821.9},
+        5: {'mono_Family': 6.0, 
+                'six_more_Family': 1.5, 
+                'low_Educ': 1.1,
+                'ass_Unease': 2.8,
+                'house_Crow': 0.7,
+                'u30_Unemployed': 7.4,
+                'eco_Unease': 1.1,
+        'tot_Dosi': regTotalVaccines(5), 
+                'residenti': 4869830,
+        'PIL' : 152340.6},
+        6: {'mono_Family': 6.5, 
+                'six_more_Family': 1.0, 
+                'low_Educ': 0.7,
+                'ass_Unease': 3.0,
+                'house_Crow': 0.6,
+                'u30_Unemployed': 7.5,
+                'eco_Unease': 1.0,
+        'tot_Dosi': regTotalVaccines(6), 
+                'residenti': 1201510,
+        'PIL' : 36814.3},
+        7: {'mono_Family': 7.7, 
+                'six_more_Family': 0.7, 
+                'low_Educ': 1.3,
+                'ass_Unease': 3.7,
+                'house_Crow': 1.2,
+                'u30_Unemployed': 9.2,
+                'eco_Unease': 1.4,
+        'tot_Dosi': regTotalVaccines(7), 
+                'residenti': 1518495,
+        'PIL' : 46194.7},
+        8: {'mono_Family': 6.8, 
+                'six_more_Family': 1.3, 
+                'low_Educ': 1.4,
+                'ass_Unease': 3.5,
+                'house_Crow': 1.1,
+                'u30_Unemployed': 8.3,
+                'eco_Unease': 1.1,
+        'tot_Dosi': regTotalVaccines(8), 
+                'residenti': 4438937,
+        'PIL' : 149633.0},
+        9: {'mono_Family': 7.0, 
+                'six_more_Family': 1.4, 
+                'low_Educ': 1.2,
+                'ass_Unease': 3.7,
+                'house_Crow': 1.2,
+                'u30_Unemployed': 9.2,
+                'eco_Unease': 1.4,
+        'tot_Dosi': regTotalVaccines(9), 
+                'residenti': 3692865,
+        'PIL' : 111605.6},
+        10: {'mono_Family': 6.7, 
+                'six_more_Family': 1.8, 
+                'low_Educ': 1.1,
+                'ass_Unease': 3.7,
+                'house_Crow': 0.7,
+                'u30_Unemployed': 8.9,
+                'eco_Unease': 1.4,
+            'tot_Dosi': regTotalVaccines(10), 
+                'residenti': 865452,
+        'PIL' : 21340.0},
+        11: {'mono_Family': 6.5, 
+                'six_more_Family': 1.8, 
+                'low_Educ': 1.1,
+                'ass_Unease': 3.9,
+                'house_Crow': 1.0,
+                'u30_Unemployed': 8.1,
+                'eco_Unease': 1.3,
+            'tot_Dosi': regTotalVaccines(11), 
+                'residenti': 1498236,
+        'PIL' : 39412.4},
+        12: {'mono_Family': 9.1,
+                'six_more_Family': 1.2, 
+                'low_Educ': 1.0,
+                'ass_Unease': 2.6,
+                'house_Crow': 1.6,
+                'u30_Unemployed': 11.9,
+                'eco_Unease': 2.5,
+            'tot_Dosi': regTotalVaccines(12), 
+                'residenti': 5730399,
+        'PIL' : 186306.9},
+        13: {'mono_Family': 6.8, 
+                'six_more_Family': 1.6, 
+                'low_Educ': 1.2,
+                'ass_Unease': 3.6,
+                'house_Crow': 0.9,
+                'u30_Unemployed': 10.3,
+                'eco_Unease': 2.0,
+            'tot_Dosi': regTotalVaccines(13), 
+                'residenti': 1281012,
+        'PIL' : 30662.3},
+        14: {'mono_Family': 6.8, 
+                'six_more_Family': 1.2, 
+                'low_Educ': 1.3,
+                'ass_Unease': 3.8,
+                'house_Crow': 1.0,
+                'u30_Unemployed': 11.5,
+                'eco_Unease': 2.5,
+            'tot_Dosi': regTotalVaccines(14), 
+                'residenti': 294294,
+        'PIL' : 6008.3},
+        15: {'mono_Family': 8.0, 
+                'six_more_Family': 2.8, 
+                'low_Educ': 2.9,
+                'ass_Unease': 2.2,
+                'house_Crow': 3.5,
+                'u30_Unemployed': 20.4,
+                'eco_Unease': 7.6,
+            'tot_Dosi': regTotalVaccines(15), 
+                'residenti': 5624260,
+        'PIL' : 102702.3},
+        16: {'mono_Family': 6.1, 
+                'six_more_Family': 1.5, 
+                'low_Educ': 2.5,
+                'ass_Unease': 3.1,
+                'house_Crow': 1.6,
+                'u30_Unemployed': 15.6,
+                'eco_Unease': 4.3,
+            'tot_Dosi': regTotalVaccines(16), 
+                'residenti': 3933777,
+        'PIL' : 70433.2},
+        17: {'mono_Family': 5.5, 
+                'six_more_Family': 1.1, 
+                'low_Educ': 1.6,
+                'ass_Unease': 3.6,
+                'house_Crow': 1.4,
+                'u30_Unemployed': 12.0,
+                'eco_Unease': 3.1,
+            'tot_Dosi': regTotalVaccines(17), 
+                'residenti': 545130,
+        'PIL' : 11480.3},
+        18: {'mono_Family': 6.5, 
+                'six_more_Family': 1.6, 
+                'low_Educ': 3.0,
+                'ass_Unease': 3.0,
+                'house_Crow': 1.5,
+                'u30_Unemployed': 17.6,
+                'eco_Unease': 5.1,
+            'tot_Dosi': regTotalVaccines(18), 
+                'residenti': 1860601,
+        'PIL' : 30759.1},
+        19: {'mono_Family': 6.4, 
+                'six_more_Family': 1.5, 
+                'low_Educ': 3.1,
+                'ass_Unease': 3.1,
+                'house_Crow': 2.1,
+                'u30_Unemployed': 19.4,
+                'eco_Unease': 5.9,
+                'tot_Dosi': regTotalVaccines(19), 
+                'residenti': 4833705,
+        'PIL' : 83065.0},
+        20: {'mono_Family': 6.8, 
+                'six_more_Family': 1.2, 
+                'low_Educ': 1.5,
+                'ass_Unease': 2.6,
+                'house_Crow': 0.9,
+                'u30_Unemployed': 12.2,
+                'eco_Unease': 3.3,
+                'tot_Dosi': regTotalVaccines(20), 
+                'residenti': 1590044,
+        'PIL' : 32121.0}}
+        with open(f'data{os.sep}regionSVI_PIL.json', 'w') as json_file:
+            json.dump(dictFactors, json_file, cls=NpEncoder, indent=4)
 
-    newDf = pd.DataFrame(values)
-    print(newDf['percentVaccinated'].sum())
-    return(newDf)
+    newDF = pd.read_json(f'data{os.sep}regionSVI_PIL.json')
 
-secondaDose = preProcessing()
-vaccines = vaccineData(secondaDose)
-
-from sklearn.linear_model import LogisticRegression
-features = ['IVSM']
-X = vaccines[features]
-y = vaccines['region']
-lr_model = LogisticRegression()
-#lr_model.fit(vaccines[['totPregressi','IVSM']].values.reshape(-1, 1), vaccines['percentVaccinated'].values.reshape(-1, 1))
-lr_model.fit(X, y)
-print(lr_model.predict(vaccines['eta6'].values.reshape(-1, 1)))"""
+    return newDF
